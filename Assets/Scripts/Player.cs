9f4c5346cf94679d4bc54fae3f2a4dd8aaeac3c7
Azarianz <62070676+Azarianz@users.noreply.github.com>
2020-06-18 00:24:41 +0800

    Updated prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public float jumpforce;
+    private Rigidbody rb;
+
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void Update()  
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.AddForce(Vector3.up * jumpforce * 1000 * Time.fixedDeltaTime);
+        }
+            
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Avoid")
+        {
+            GameManager.thisManager.UpdateScore(1);
+        }
+    }
+
+
 }
